fb99ec5c23fe62d30833597c170be849032b11bc
oraerk <oraerk@gmail.com>
2021-10-19 00:05:26 +0300

    simplify 1

--- setup.sh
+++ setup.sh
@@ -52,33 +52,8 @@ if [[ "${IP}" != "${VPNHOSTIP}" ]]; then
 fi
 
 read -r -p "VPN username: " VPNUSERNAME
-while true; do
 read -r -s -p "VPN password (no quotes, please): " VPNPASSWORD
-echo
-read -r -s -p "Confirm VPN password: " VPNPASSWORD2
-echo
-[[ "${VPNPASSWORD}" = "${VPNPASSWORD2}" ]] && break
-echo "Passwords didn't match -- please try again"
-done
 
-echo '
-Public DNS servers include:
-
-176.103.130.130,176.103.130.131  AdGuard               https://adguard.com/en/adguard-dns/overview.html
-176.103.130.132,176.103.130.134  AdGuard Family        https://adguard.com/en/adguard-dns/overview.html
-1.1.1.1,1.0.0.1                  Cloudflare/APNIC      https://1.1.1.1
-84.200.69.80,84.200.70.40        DNS.WATCH             https://dns.watch
-8.8.8.8,8.8.4.4                  Google                https://developers.google.com/speed/public-dns/
-208.67.222.222,208.67.220.220    OpenDNS               https://www.opendns.com
-208.67.222.123,208.67.220.123    OpenDNS FamilyShield  https://www.opendns.com
-9.9.9.9,149.112.112.112          Quad9                 https://quad9.net
-77.88.8.8,77.88.8.1              Yandex                https://dns.yandex.com
-77.88.8.88,77.88.8.2             Yandex Safe           https://dns.yandex.com
-77.88.8.7,77.88.8.3              Yandex Family         https://dns.yandex.com
-'
-
-#read -r -p "DNS servers for VPN users (default: 1.1.1.1,1.0.0.1): " VPNDNS
-#VPNDNS=${VPNDNS:-'1.1.1.1,1.0.0.1'}
 VPNDNS='1.1.1.1,1.0.0.1'
 
 
